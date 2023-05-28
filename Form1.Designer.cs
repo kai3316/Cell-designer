9f550b9c473ee087de0d65c976a393cb6b1b621e
Kai <sukai3316@gmail.com>
2023-05-29 03:06:23 +0900

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Cell_designer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(252, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 92);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear all annotation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(496, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(224, 92);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Clear current annotation";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(740, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 92);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Save annotation";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(989, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(224, 92);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Rectangle mode";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(1491, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(224, 92);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Export configuration";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 121);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1703, 885);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(1325, 787);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(243, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 1021);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 1021);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1727, 1057);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Cell designer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,422 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Drawing.Drawing2D;
+using System.Reflection.Emit;
+using System.IO;
+
+namespace Cell_designer
+{
+    public partial class Form1 : Form
+    {
+
+        private int clickCount = 0;
+        private Point startPoint;
+        bool isRectangleMarked = true;
+        System.Drawing.Rectangle rectangle;
+        JArray marks = new JArray();
+        JArray marksForsave = new JArray();
+        private PointF[] PolygonPoints = new PointF[0];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isRectangleMarked)
+            {
+                //Console.WriteLine(clickCount);
+                // 增加计数器
+                clickCount++;
+                // 如果是第一次单击
+                if (clickCount == 1)
+                {
+                    // 记录起点
+                    startPoint = e.Location;
+                }
+                // 如果是第二次单击
+                else if (clickCount == 2)
+                {
+                    // 计算矩形的位置和大小
+                    int x = Math.Min(startPoint.X, e.X);
+                    int y = Math.Min(startPoint.Y, e.Y);
+                    int width = Math.Abs(startPoint.X - e.X);
+                    int height = Math.Abs(startPoint.Y - e.Y);
+                    // 更新 rectangle 变量
+                    rectangle = new System.Drawing.Rectangle(x, y, width, height);
+                    // 触发 PictureBox 的重绘事件
+                    pictureBox1.Invalidate();
+                    // 重置计数器
+                    clickCount = 0;
+                }
+            }
+            else
+            {
+
+                Point point = new Point(e.X, e.Y);
+
+                // 将鼠标单击的位置添加到 PolygonPoints 数组中
+                Array.Resize(ref PolygonPoints, PolygonPoints.Length + 1);
+                PolygonPoints[PolygonPoints.Length - 1] = point;
+                // 如果 PolygonPoints 数组中的点的数量大于等于 3 个，表示多边形已经被完整标记
+                pictureBox1.Invalidate();
+
+            }
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            // 设置 picturebox2 的位置为鼠标的当前位置
+            pictureBox2.Enabled = true;
+
+            Point pt = new Point();
+            pt.X = Cursor.Position.X - this.Location.X;
+            pt.Y = Cursor.Position.Y - this.Location.Y;
+            pictureBox2.Location = pt;
+            // 设置 picturebox2 的可见性为 true
+            pictureBox2.Visible = true;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox2.Enabled = false;
+            label1.Text = "X";
+            label2.Text = "Y";
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int originalHeight = this.pictureBox1.Image.Height;
+
+            PropertyInfo rectangleProperty = this.pictureBox1.GetType().GetProperty("ImageRectangle", BindingFlags.Instance | BindingFlags.NonPublic);
+            Rectangle picrectangle = (Rectangle)rectangleProperty.GetValue(this.pictureBox1, null);
+
+            int currentWidth = picrectangle.Width;
+            int currentHeight = picrectangle.Height;
+
+            double rate = (double)currentHeight / (double)originalHeight;
+
+            int black_left_width = (currentWidth == this.pictureBox1.Width) ? 0 : (this.pictureBox1.Width - currentWidth) / 2;
+            int black_top_height = (currentHeight == this.pictureBox1.Height) ? 0 : (this.pictureBox1.Height - currentHeight) / 2;
+            int zoom_x = e.X - black_left_width;
+            int zoom_y = e.Y - black_top_height;
+
+            int original_x = (int)(zoom_x / rate);
+            int original_y = (int)(zoom_y / rate);
+            label1.Text = original_x.ToString();
+            label2.Text = original_y.ToString();
+            int width_g = 100; // 放大的宽度
+            int height_g = 100; // 放大的高度
+
+            Bitmap bmp = new Bitmap(width_g, height_g);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                g.DrawImage(pictureBox1.Image, new Rectangle(0, 0, width_g * 2, height_g * 2), new Rectangle(original_x - 25, original_y - 25, width_g, height_g), GraphicsUnit.Pixel);
+                g.FillEllipse(Brushes.Yellow, width_g / 2 - 2, height_g / 2 - 2, 4, 4);
+            }
+            pictureBox2.Image = bmp;
+            Point pt = new Point();
+            if (Cursor.Position.X + width_g * 2 > this.Width) // 当鼠标位置右侧空间不足时
+            {
+                // 在鼠标左侧显示
+                pt.X = Cursor.Position.X - this.Location.X - width_g * 2 - 10;
+            }
+            else
+            {
+                // 在鼠标右侧显示
+                pt.X = Cursor.Position.X - this.Location.X + 10;
+            }
+
+            if (Cursor.Position.Y + height_g * 2 > this.Height) // 当鼠标位置下方空间不足时
+            {
+                // 在鼠标上方显示
+                pt.Y = Cursor.Position.Y - this.Location.Y - height_g * 2 - 20;
+            }
+            else
+            {
+                // 在鼠标下方显示
+                pt.Y = Cursor.Position.Y - this.Location.Y + 20;
+            }
+
+            pictureBox2.Location = pt;
+
+
+            if (clickCount == 1)
+            {
+                // 计算矩形的位置和大小
+                int x = Math.Min(startPoint.X, e.X);
+                int y = Math.Min(startPoint.Y, e.Y);
+                int width = Math.Abs(startPoint.X - e.X);
+                int height = Math.Abs(startPoint.Y - e.Y);
+                // 更新 rectangle 变量
+                rectangle = new Rectangle(x, y, width, height);
+                // 触发 PictureBox 的重绘事件
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // 获取 PictureBox 的画布
+            Graphics g = e.Graphics;
+
+            // 遍历 marks 数组
+            foreach (JArray mark in marks)
+            {
+                // 获取标记形状
+                string shape = mark[0].ToString();
+                // 如果是矩形
+                if (shape == "rectangle")
+                {
+                    // 获取矩形的位置和大小
+                    int x = mark[1].ToObject<int>();
+                    int y = mark[2].ToObject<int>();
+                    int width = mark[3].ToObject<int>();
+                    int height = mark[4].ToObject<int>();
+                    // 画出矩形
+                    g.DrawRectangle(Pens.Red, new Rectangle(x, y, width, height));
+                }
+                // 如果是多边形
+                else if (shape == "polygon")
+                {
+                    // 获取多边形的顶点坐标
+                    float[] listp = new float[mark.Count() - 1];
+
+                    for (int i = 1; i < mark.Count(); i++)
+                    {
+                        listp[i - 1] = float.Parse(mark[i].ToString());
+                    }
+
+                    PointF[] points = new PointF[listp.Length / 2];
+                    for (int i = 0; i < listp.Length; i += 2)
+                    {
+                        points[i / 2] = new PointF((float)listp[i], (float)listp[i + 1]);
+                    }
+                    // 画出多边形
+                    g.DrawPolygon(Pens.Red, points);
+                }
+            }
+
+
+            g.DrawRectangle(Pens.Red, rectangle);
+            if (PolygonPoints != null)
+            {
+                if (PolygonPoints.Length < 3)
+                {
+                    // 遍历 points 数组
+                    foreach (PointF point in PolygonPoints)
+                    {
+                        // 在 PictureBox 中显示点的位置
+                        g.FillEllipse(Brushes.Red, point.X - 2, point.Y - 2, 4, 4);
+                    }
+                }
+                // 如果 points 数组中的点的数量大于等于 3
+                else
+                {
+                    // 画出多边形
+                    g.DrawPolygon(Pens.Red, PolygonPoints);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = openFileDialog.FileName;
+                    pictureBox1.Load(fileName);
+                    pictureBox1.Enabled = true;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clickCount = 0;
+            rectangle = new Rectangle();
+            PolygonPoints = new PointF[0];
+            marks = new JArray();
+            marksForsave = new JArray();
+            pictureBox1.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            clickCount = 0;
+            rectangle = new Rectangle();
+            PolygonPoints = new PointF[0];
+            pictureBox1.Invalidate();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.pictureBox1.Image == null)
+            {
+                return;
+            }
+            int originalHeight = this.pictureBox1.Image.Height;
+            int originalWidth = this.pictureBox1.Image.Width;
+
+            PropertyInfo rectangleProperty = this.pictureBox1.GetType().GetProperty("ImageRectangle", BindingFlags.Instance | BindingFlags.NonPublic);
+            Rectangle picturerectangle = (Rectangle)rectangleProperty.GetValue(this.pictureBox1, null);
+
+            int currentWidth = picturerectangle.Width;
+            int currentHeight = picturerectangle.Height;
+
+            float rate = (float)currentHeight / (float)originalHeight;
+
+            int black_left_width = (currentWidth == this.pictureBox1.Width) ? 0 : (this.pictureBox1.Width - currentWidth) / 2;
+            int black_top_height = (currentHeight == this.pictureBox1.Height) ? 0 : (this.pictureBox1.Height - currentHeight) / 2;
+
+
+            if (isRectangleMarked)
+            {
+                int zoom_x = rectangle.X - black_left_width;
+                int zoom_y = rectangle.Y - black_top_height;
+
+                float original_x = (float)zoom_x / rate;
+                float original_y = (float)zoom_y / rate;
+                if (original_x < 0)
+                {
+                    original_x = 0;
+
+                }
+                if (original_y < 0)
+                {
+                    original_y = 0;
+                }
+
+                if (original_x > this.pictureBox1.Image.Width)
+                {
+                    original_x = this.pictureBox1.Image.Width;
+
+                }
+                if (original_y >= this.pictureBox1.Image.Height)
+                {
+                    original_y = this.pictureBox1.Image.Height;
+                }
+
+                float original_width = rectangle.Width / rate;
+                float original_height = rectangle.Height / rate;
+                // 将矩形标记信息添加到 JArray 中
+                marksForsave.Add(new JArray("rectangle", original_x / originalWidth, original_y / originalHeight, original_width / originalWidth, original_height / originalHeight));
+                marks.Add(new JArray("rectangle", rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height));
+                pictureBox1.Invalidate();
+                clickCount = 0;
+            }
+            // 如果当前是多边形标记状态
+            else
+            {
+                if (PolygonPoints == null || PolygonPoints.Length < 3)
+                {
+                    return;
+                }
+                JArray pointArray = new JArray("polygon");
+                JArray pointArraySave = new JArray("polygon");
+                foreach (PointF point in PolygonPoints)
+                {
+                    pointArray.Add(point.X);
+                    pointArray.Add(point.Y);
+                    float zoom_x = point.X - black_left_width;
+                    float zoom_y = point.Y - black_top_height;
+
+                    float original_x = (float)zoom_x / rate;
+                    float original_y = (float)zoom_y / rate;
+                    if (original_x < 0)
+                    {
+                        original_x = 0;
+
+                    }
+                    if (original_y < 0)
+                    {
+                        original_y = 0;
+                    }
+
+                    if (original_x > this.pictureBox1.Image.Width)
+                    {
+                        original_x = this.pictureBox1.Image.Width;
+
+                    }
+                    if (original_y > this.pictureBox1.Image.Height)
+                    {
+                        original_y = this.pictureBox1.Image.Height;
+                    }
+                    pointArraySave.Add(original_x / originalWidth);
+                    pointArraySave.Add(original_y / originalHeight);
+                }
+                // 将多边形标记信息添加到 JArray 中
+                marks.Add(pointArray);
+                marksForsave.Add(pointArraySave);
+                pictureBox1.Invalidate();
+                PolygonPoints = new PointF[0];
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (isRectangleMarked)
+            {
+                // 将 isRectangleMarked 标志设置为 true
+                isRectangleMarked = false;
+
+                // 更新按钮文本
+                button5.Text = "Polygon mode";
+            }
+            else
+            {
+                // 将 isRectangleMarked 标志设置为 true
+                isRectangleMarked = true;
+                // 更新按钮文本
+                button5.Text = "Rectangle mode";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            JObject output = new JObject { { "Labels", new JObject { { "LabelList", new JArray("Class 1", "Class 2") }, { "LabelAbbreviation", new JArray("C1", "C2") }, { "LabelNumber", "2" } } }, { "COIs", new JObject { { "COINumber", marksForsave.Count.ToString() } } } };
+
+            // 遍历JArray数组，将每个数组元素添加到JSON对象中
+            for (int i = 0; i < marksForsave.Count; i++)
+            {
+                output["COIs"][(i + 1).ToString()] = marksForsave[i];
+            }
+
+            // 将JSON对象转换为字符串
+            string outputStr = output.ToString();
+            SaveFileDialog saveDialog = new SaveFileDialog();
+
+            // 设置文件保存位置（这里设置为桌面）
+            saveDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveDialog.FileName = "new configuration.json";
+            saveDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+
+            // 设置默认文件保存类型为JSON文件
+            saveDialog.DefaultExt = "json";
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 使用File类的WriteAllText方法将JSON字符串保存到文件中
+                File.WriteAllText(saveDialog.FileName, outputStr);
+            }
+
+            Console.WriteLine(outputStr);
+        }
+    }
+}
